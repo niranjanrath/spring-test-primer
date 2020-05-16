dff2075a09a83c62a0fdbef11f2df5c98eeb5d01
Niranjan <niranjan.rath@rabobank.nl>
2020-05-16 15:05:05 +0200

    first commit

---
+++ src/main/java/nl/niranjan/spring/testprimer/SpringTestPrimerApplication.java
@@ -0,0 +1,13 @@
+package nl.niranjan.spring.testprimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestPrimerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestPrimerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/niranjan/spring/testprimer/SpringTestPrimerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.niranjan.spring.testprimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestPrimerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
