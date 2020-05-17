298977ecc0977c2621ba7cd9e7f930db50e7e6b2
Niranjan <niranjan.rath@rabobank.nl>
2020-05-17 18:49:14 +0200

    fixed the 10min technical debt

--- src/test/java/nl/niranjan/spring/testprimer/SpringTestPrimerApplicationTests.java
+++ src/test/java/nl/niranjan/spring/testprimer/SpringTestPrimerApplicationTests.java
@@ -1,13 +1,31 @@
 package nl.niranjan.spring.testprimer;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringTestPrimerApplicationTests {
 
+    @Autowired
+    private EmployeeController employeeController;
+
+    @Autowired
+    private EmployeeBusiness employeeBusiness;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Test
     void contextLoads() {
+        Assertions.assertThat(employeeController).isNotNull();
+        Assertions.assertThat(employeeBusiness).isNotNull();
+        Assertions.assertThat(employeeService).isNotNull();
+        Assertions.assertThat(employeeRepository).isNotNull();
     }
 
 }
