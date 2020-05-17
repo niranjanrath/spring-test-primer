80321c04e531a74c2b0c2371d4fa9a66eeec34e7
Niranjan <niranjan.rath@rabobank.nl>
2020-05-17 18:57:25 +0200

    fixed the security issue technical debt

--- src/main/java/nl/niranjan/spring/testprimer/SpringTestPrimerApplication.java
+++ src/main/java/nl/niranjan/spring/testprimer/SpringTestPrimerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringTestPrimerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringTestPrimerApplication.class, args);
+        SpringApplication.run(SpringTestPrimerApplication.class);
     }
 
 }
